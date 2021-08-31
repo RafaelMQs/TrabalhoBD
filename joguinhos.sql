c32e2c43f0d883ca6ee5221ddc2f750bc394288c
RafaelMQs <rafaeelmq08@gmail.com>
2021-08-31 16:17:48 -0300

    Criando projeto do Bando de Dados

---
+++ joguinhos.sql
@@ -0,0 +1,40 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 31-Ago-2021 às 21:16
+-- Versão do servidor: 10.4.19-MariaDB
+-- versão do PHP: 8.0.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `joguinhos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `jogos`
+--
+
+CREATE TABLE `jogos` (
+  `nomeJogo` tinytext DEFAULT NULL,
+  `tipoJogo` tinytext DEFAULT NULL,
+  `playersJogo` int(25) DEFAULT NULL,
+  `dataJogo` int(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
